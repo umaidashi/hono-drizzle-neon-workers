974396f0c06e5d09e40d77bdfe7f174cfc62b663
umaidashi <umaidashi18@gmail.com>
2025-01-19 20:47:23 +0900

    Remove console log and static message from root API endpoint in index.ts, ensuring the endpoint solely returns user data from the database.

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,9 @@ app.use("*", dbMiddleware);
 
 app.get("/", async (c) => {
 	const db = c.get("db");
-	console.log(db);
 	const users = await db.select().from(usersTable);
 
 	return c.json(users);
-	// return c.json({ message: "Hello World" });
 });
 
 export default app;
